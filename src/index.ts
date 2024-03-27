f16014ea18efc8e6a7d921a5aadb654423cd0ae6
Matheus <matheusbastosandrade@gmail.com>
2024-03-27 18:42:55 -0300

    feat: relacionamento e aprendendo sobre os joins

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,38 @@ const sql = postgres("postgres://postgres:root@localhost:5432/sql", {
 
 async function createTableUsers() {
   await sql/*sql*/ `
-  CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY, 
-    nickname TEXT NOT NULL UNIQUE, 
-    user_password TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY, 
+      nickname TEXT NOT NULL UNIQUE, 
+      user_password TEXT NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )`;
-
-  console.log("conexão fechada");
 }
 
+// criando relacionamento entre produto e categoria
+async function createTableProducts() {
+  await sql/*sql*/ `
+    CREATE TABLE IF NOT EXISTS products (
+      id SERIAL PRIMARY KEY,
+      name_product TEXT NOT NULL UNIQUE,
+      price_product INTEGER NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      id_category INTEGER REFERENCES categories(id)
+    )
+  `;
+}
+
+async function createTableCategory() {
+  await sql/*sql*/ `
+    CREATE TABLE IF NOT EXISTS categories (
+      id SERIAL PRIMARY KEY,
+      name_category TEXT UNIQUE,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  )`;
+}
+
+createTableCategory();
+createTableProducts();
 createTableUsers();
 
 export { sql };
--- src/server.ts
+++ src/server.ts
@@ -77,6 +77,45 @@ app.delete("/users/:id", async (req: Request, res: Response) => {
   return res.status(200).json({ user });
 });
 
+app.post("/categories", async (req: Request, res: Response) => {
+  const { name_category } = req.body;
+
+  // criando uma categoria, passando o nome
+  const category = await sql/*sql*/ `
+    INSERT INTO categories (name_category)
+    VALUES (${name_category})
+
+    RETURNING *
+  `;
+
+  return res.status(200).json({ category });
+});
+
+app.post("/products", async (req: Request, res: Response) => {
+  const { name_product, price_product, id_category } = req.body;
+
+  // criando um produto, passando nome, preço e id da categoria
+  const product = await sql/*sql*/ `
+    INSERT INTO products (name_product, price_product, id_category)
+    VALUES (${name_product}, ${price_product}, ${id_category})
+
+    RETURNING *
+  `;
+
+  return res.status(200).json({ product });
+});
+
+app.get("/products", async (req: Request, res: Response) => {
+  // selecionando as colunas de nome do produto, preço do produto e pegando na tabela categories o nome da categoria correspondente ao id_category (relacionamento de produtos com categorias).
+  const products = await sql/*sql*/ `
+    SELECT name_product, price_product, name_category
+    FROM products
+    INNER JOIN categories ON products.id_category = categories.id
+  `;
+
+  return res.status(200).json({ products });
+});
+
 app.listen(3333, () => {
   console.log("server rodando na porta 3333");
 });
